47086ec68474a68d21eb73fb96389514318e351a
sajeevan <sajeevanjspy@gmail.com>
2026-03-11 00:33:22 +0530

    feat: renderer interfaces

---
+++ src/renderer/renderer.h
@@ -0,0 +1,64 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "core/error.h"
+#include "vk_defines.h"
+
+typedef struct {
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debug_messenger;
+    VkPhysicalDevice gpu;
+    VkPhysicalDeviceMemoryProperties gpu_mem_props;
+    VkDevice device;
+    VkSurfaceKHR surface;
+    VkQueue graphics_queue;
+    VkQueue present_queue;
+    uint32_t graphics_family;
+    uint32_t present_family;
+} Core;
+
+typedef struct {
+    VkSwapchainKHR swapchain;
+    VkFormat format;
+    VkExtent2D extent;
+    uint32_t image_count;
+    VkImage* images;
+    VkImageView* image_views;
+    VkRenderPass render_pass;
+    VkFramebuffer* framebuffers;
+    // VkImage depth_image;
+    // VkDeviceMemory depth_memory;
+    // VkImageView depth_image_view;
+} Swapchain;
+
+typedef struct {
+    VkCommandPool command_pool;
+    VkCommandBuffer* command_buffers;
+    VkSemaphore image_available_semaphore;
+    VkSemaphore render_finished_semaphore;
+    VkFence in_flight_fence;
+} FrameData;
+
+typedef struct {
+    Core core;
+    Swapchain swapchain;
+    FrameData frame;
+} Renderer;
+
+// core
+Core* core_init(GLFWwindow* window);
+void core_destroy(Core* core);
+
+// swapchain
+AppResult swapchain_create();
+void swapchain_destroy();
+
+// frame data
+AppResult frame_data_create();
+void frame_data_destroy();
+
+// renderer
+AppResult renderer_create();
+void renderer_destroy();
+
+#endif  // RENDERER_H
