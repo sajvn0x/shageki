c7493407cddd00416d80086df68370211ea44b5d
sajeevan <sajeevanjspy@gmail.com>
2026-03-11 11:48:18 +0530

    refactor: renderer - managing memory properly

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -1,6 +1,7 @@
 #include "renderer.h"
 
 #include <GLFW/glfw3.h>
+#include <assert.h>
 #include <string.h>
 
 #include "core/error.h"
@@ -10,36 +11,49 @@ Renderer* renderer_init(GLFWwindow* window) {
 
     Renderer* renderer = malloc(sizeof(Renderer));
     if (!renderer) {
-        LOG_ERROR("RENDERER_CORE: Failed to create\n");
+        LOG_ERROR("RENDERER: failed to allocate renderer\n");
         return NULL;
     }
-    renderer = memset(renderer, 0, sizeof(Renderer));
+    memset(renderer, 0, sizeof(Renderer));
 
-    Core* core = core_init(window);
-    if (!core) {
-        LOG_ERROR("RENDERER_CORE: Failed to create\n");
+    // Core initialization
+    renderer->core = core_init(window);
+    if (!renderer->core) {
+        LOG_ERROR("RENDERER: core initialization failed\n");
+        free(renderer);
         return NULL;
     }
-    renderer->core = core;
 
-    AppResult swapchain_result = swapchain_init(renderer, window);
-    if (swapchain_result != APP_SUCCESS) return NULL;
+    // Swapchain initialization
+    if (swapchain_init(renderer, window) != APP_SUCCESS) {
+        LOG_ERROR("RENDERER: swapchain initialization failed\n");
+        core_destroy(renderer->core);
+        free(renderer->core);
+        free(renderer);
+        return NULL;
+    }
+
+    assert(renderer->core);
+    assert(renderer->swapchain);
 
     LOG_INFO("RENDERER: initialized\n");
     return renderer;
 }
 
 void renderer_destroy(Renderer* renderer) {
+    if (!renderer) return;
     LOG_INFO("RENDERER: destructing\n");
 
     if (renderer->swapchain) {
         swapchain_destroy(renderer->swapchain);
         free(renderer->swapchain);
+        renderer->swapchain = NULL;
     }
 
     if (renderer->core) {
         core_destroy(renderer->core);
         free(renderer->core);
+        renderer->core = NULL;
     }
 
     LOG_INFO("RENDERER: destructed\n");
